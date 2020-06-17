0057cba25839f815c3d10533cfb6821dd089e83f
Johnnyr81 <emailjohn81@gmail.com>
2020-06-16 20:56:04 -0500

    update

--- wunderlist/src/components/Register.js
+++ wunderlist/src/components/Register.js
@@ -1,34 +1,80 @@
 import React, {useState} from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { Col, Button, Row, Form, FormGroup, Input } from 'reactstrap';
+
 
 function Register(props) {
     const [user, setUser] = useState({first_name: '', last_name: '', email: '', password: ''});
 
     const handleChanges = e => {
+        
         setUser({ ...user, [e.target.name]: e.target.value });
       };
 
     const submitForm = e => {
         e.preventDefault();
+
+        ///added for form validation demo
+        let message = ""
+        if (user.password.length <= 6){
+            message='Password must be longer than 6 characters'
+        }
+
+        if (message) {
+            setUser({ ...user, message });
+            return 
+        }
+        ///added for form validation demo
+
         axiosWithAuth().post('https://wunderlist-2-0-be.herokuapp.com/api/auth/register', user)
             .then(response => {
                 props.history.push('/login')
             })
             .catch(error => {
+                e.preventDefault();
                 console.log(error)
                 setUser({first_name: '', last_name: '', email: '', password: ''})
+                
             })
+
+            
     };
 
     return (
         <div>
-            <form onSubmit={submitForm}>
-            <input type="text" name="first_name" value={user.first_name} placeholder="First Name" onChange={handleChanges} />
-            <input type="text" name="last_name" value={user.last_name} placeholder="Last Name" onChange={handleChanges} />
-            <input type="text" name="email" value={user.email} placeholder="Email Address" onChange={handleChanges} />
-            <input type="password" name="password" value={user.password} placeholder="Password" onChange={handleChanges} />
-            <button type="submit">Submit</button>
-            </form>
+            <Form onSubmit={submitForm} style={{margin: "5% 25%"}}>
+            <h2>Register</h2>
+            <Row form>
+                <Col md={3}>
+                <FormGroup>
+                    <Input type="text" name="first_name" id="first_name" placeholder="First Name"  value={user.first_name} onChange={handleChanges} />
+                </FormGroup>
+                </Col>
+                <Col md={3}>
+                <FormGroup>
+                    <Input type="text" name="last_name" id="last_name" placeholder="Last Name" value={user.last_name} onChange={handleChanges} />
+                </FormGroup>
+                </Col>
+            </Row>
+            <FormGroup row>
+                <Col sm={8}>
+                <Input type="email" name="email" id="email" placeholder="Email Address" value={user.email} onChange={handleChanges} />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Col sm={8}>
+                <Input type="password" name="password" id="password" placeholder="Password" value={user.password} onChange={handleChanges} />
+                {/* added for form validation demo */}
+                <div>{user.message}</div>
+                {/* added for form validation demo */}
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Col>
+                <Button type="submit" style={{backgroundColor: "#73628A"}}>Submit</Button>
+                </Col>
+            </FormGroup>
+            </Form>
         </div>
     )
 }
